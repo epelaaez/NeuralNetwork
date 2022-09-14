b69a9349972a9537a65c9993b7ff038bd9f9c697
Emilio Pelaez <epelaaez@gmail.com>
2022-09-14 13:44:07 -0500

    add python script to visualize data

---
+++ visualize.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+with open("data/train.csv") as file:
+    file = list(file)
+    data = [int(i.replace("\n", "")) for i in file[89].split(",")] # index i of file[i] chooses the image to display
+    print(data)
+data = np.array(data)
+data = np.resize(data, (32, 32))
+data = data.T
+plt.imshow(data, cmap='gray', vmin=0, vmax=255)
+plt.show()
